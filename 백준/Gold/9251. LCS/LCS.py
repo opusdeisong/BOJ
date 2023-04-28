d68f1a22048fd6b159cdbbe4326dced78a4def1b
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 19:34:27 +0900

    [Gold V] Title: LCS, Time: 556 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,13 @@
+import sys
+
+word1 = sys.stdin.readline().rstrip()
+word2 = sys.stdin.readline().rstrip()
+dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+
+for i in range(1, len(word1) + 1):
+    for j in range(1, len(word2) + 1):
+        if word1[i - 1] == word2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+print(dp[-1][-1])
