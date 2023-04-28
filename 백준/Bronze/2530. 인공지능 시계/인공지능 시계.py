b5a9d5bdf5fc572e5db8bf0e004d9df5d857dc50
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 19:23:17 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -0,0 +1,19 @@
+A, B, C = map(int, input().split())
+D = int(input())
+
+B = B + D // 60
+C = C + D % 60
+
+if C >= 60:
+    B = B + C // 60
+    C = C % 60
+
+if B >= 60:
+    A = A + B // 60
+    B = B % 60
+
+
+if A >= 24:
+    A = A % 24
+
+print(f"{A} {B} {C}")
