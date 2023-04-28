3e1a792c82135da0e62d02b8871bc802b3faa406
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 19:41:47 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i = i + 1){
+        int num1, num2;
+        scanf("%d %d", &num1, &num2);
+        printf("Case #%d: %d\n",i + 1, num1 + num2);
+    }
+}
