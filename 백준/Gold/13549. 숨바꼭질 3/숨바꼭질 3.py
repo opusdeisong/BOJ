d4718309e712e01ad565dd9c9087f5b40d4b081e
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 19:47:46 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 35352 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+N, K = map(int, sys.stdin.readline().split())
+MAX = 100001
+dist = [-1 for _ in range(MAX)]
+
+q = deque()
+q.append(N)
+dist[N] = 0
+
+while q:
+    temp = q.popleft()
+    if temp == K:
+        break
+    if 0 <= temp - 1 and dist[temp - 1] == -1:
+        dist[temp - 1] = dist[temp] + 1
+        q.append(temp - 1)
+    if temp * 2 < 100001 and dist[temp * 2] == -1:
+        dist[temp * 2] = dist[temp]
+        q.append(temp * 2)
+    if temp + 1 < 100001 and dist[temp + 1] == - 1:
+        dist[temp + 1] = dist[temp] + 1
+        q.append(temp + 1)
+print(dist[K])
