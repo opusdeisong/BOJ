5015e87051657500288c963ef6f2e8a30e31ef81
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 19:12:25 +0900

    [Platinum V] Title: 고층 빌딩, Time: 572 ms, Memory: 45448 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.py
@@ -0,0 +1,13 @@
+import sys
+
+N, L, R = map(int, sys.stdin.readline().split())
+temp = 1000000007
+
+dp = [[[0 for _ in range(101)] for _ in range(101)] for _ in range(101)]
+dp[1][1][1] = 1
+
+for i in range(2, N + 1):
+    for j in range(1, L + 1):
+        for k in range(1, R + 1):
+            dp[i][j][k] = (dp[i - 1][j][k - 1] +dp[i - 1][j - 1][k] + dp[i - 1][j][k] * (i - 2)) % temp
+print(dp[N][L][R])
