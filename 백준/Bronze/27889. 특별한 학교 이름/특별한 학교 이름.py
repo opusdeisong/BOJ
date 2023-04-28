c8506bea46b813a1ac4ec8120a6dd46edc233706
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 20:05:59 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,5 @@
+import sys
+
+dic = {"NLCS":"North London Collegiate School", "BHA":"Branksome Hall Asia", "KIS":"Korea International School", "SJA":"St. Johnsbury Academy"}
+
+print(dic[sys.stdin.readline().rstrip()])
