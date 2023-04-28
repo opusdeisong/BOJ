716b35eb0d35a714989e2e81b42c6c6aa4325176
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-04-28 20:03:46 +0900

    [Bronze V] Title: Serca, Time: 432 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,11 @@
+T = int(input())
+for _ in range(T):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
