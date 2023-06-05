7d847d2fffbe678d240952a34c75ddd1ae5d1569
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-06-06 06:45:02 +0900

    [Bronze III] Title: 세 수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -0,0 +1,20 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+if a + b == c:
+    print(f"{a}+{b}={c}")
+elif a - b == c:
+    print(f"{a}-{b}={c}")
+elif a * b == c:
+    print(f"{a}*{b}={c}")
+elif a / b == c:
+    print(f"{a}/{b}={c}")
+elif a == b + c:
+    print(f"{a}={b}+{c}")
+elif a == b - c:
+    print(f"{a}={b}-{c}")
+elif a == b * c:
+    print(f"{a}={b}*{c}")
+elif a == b / c:
+    print(f"{a}={b}/{c}")
