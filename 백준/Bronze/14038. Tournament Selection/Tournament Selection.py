55d113b23e05a3b111e357231befa92ea6422336
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-06-06 07:14:37 +0900

    [Bronze IV] Title: Tournament Selection, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14038. Tournament Selection/Tournament Selection.py
@@ -0,0 +1,15 @@
+import sys
+
+results = [sys.stdin.readline().rstrip() for _ in range(6)]
+
+wins = results.count('W')
+
+if wins >= 5:
+    print(1)
+elif wins >= 3:
+    print(2)
+elif wins >= 1:
+    print(3)
+else:
+    print(-1)
+
