c898b712a5daef709b2e7a1aac9098e907471fe3
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-06-06 06:55:20 +0900

    [Bronze IV] Title: Bałwanek, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8718. Bałwanek/Bałwanek.py
@@ -0,0 +1,11 @@
+import sys
+
+x, k = map(int, sys.stdin.readline().split())
+if k * 7 <= x:
+    print(k * 7000)
+elif k * 3.5 <= x:
+    print(k * 3500)
+elif k * 1.75 <= x:
+    print(k * 1750)
+else:
+    print(0)
