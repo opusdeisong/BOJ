299e682c81646953b172678df53a8fe7b8f325a3
Woojung, Song <105193807+opusdeisong@users.noreply.github.com>
2023-06-07 06:08:10 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4276 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -0,0 +1,12 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    n, m = map(int, sys.stdin.readline().split())
+    count = 0
+    for a in range(1, n):
+        for b in range(a+1, n):
+            if (a**2 + b**2 + m) % (a * b) == 0:
+                count += 1
+    print(count)
